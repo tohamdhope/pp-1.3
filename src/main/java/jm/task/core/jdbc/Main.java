75f3185e30eecead54a988cdb195d48d0b2d3c7f
Antony Voloshin <49843612+tohamdhope@users.noreply.github.com>
2022-06-09 22:56:59 +0300

    got it

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Antony", "Soprano", (byte) 42);
+        userService.saveUser("Elliot", "Sanders", (byte) 36);
+        userService.saveUser("Evgeny", "Fedorov", (byte) 28);
+        userService.saveUser("Artyem", "Pirogov", (byte) 32);
+        System.out.println("+-------+---------------+--------------------+----------+");
+        System.out.println("|   id  |     name      |    lastName        |    age   |");
+        System.out.println("+-------+---------------+--------------------+----------+");
+        userService.getAllUsers();
+        //userService.cleanUsersTable();
+        //userService.dropUsersTable();
     }
 }
